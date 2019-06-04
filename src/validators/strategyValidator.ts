da7e138910aee7cc4baaa213c91d053fba3f7a39
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-04 11:35:42 +0200

    DRYed up the strategyValidator a bit

--- src/validators/strategyValidator.ts
+++ src/validators/strategyValidator.ts
@@ -1,6 +1,5 @@
-import { IValidator } from './IValidator.interface'
-// import { IResultHandler } from '~src/utils/resultHandler'
-import { isValidated } from './validator-modules/isValidated'
+import { IValidator } from "./IValidator.interface"
+import { isValidated } from "./validator-modules/isValidated"
 
 interface IStrategyValidator {
   readonly validators: IValidator[]
@@ -8,44 +7,30 @@ interface IStrategyValidator {
   execute(): boolean
 }
 
+type ValidatorReturnType = { result: boolean; errors: Error[] | [] }
+
 /**
  * Runs all supplied Validators in order and determines if a Strategy is qualified to be submitted to storage.
  * ~param validators: Array of all Validators to run, exposing an `execute()` method to comply with the IValidator contract.
  * ~returns A `boolean` value
  */
 export function strategyValidator(validators: IValidator[]): IValidator {
-  async function execute(): Promise<{ result: boolean; errors: Error[] | [] }> {
-    const resultPromises: Promise<{ result: boolean; errors: Error[] | [] }>[] = validators.map((validator: IValidator) =>
-      validator.execute()
+  async function execute(): Promise<ValidatorReturnType> {
+    const resultPromises: Promise<ValidatorReturnType>[] = validators.map(
+      (validator: IValidator) => validator.execute()
     )
-
-    // const errors: Error[] = await Promise.all(resultPromises)
-    //   .then((values: { result: boolean; errors: Error[] }[]) => {
-    //     return values.map(({ errors }) => errors).reduce((e) => e)
-    //   })
-    //   .catch((err: Error) => {
-    //     console.log(err)
-
-    //     return [] as Error[]
-    //   })
-
-    // const results: { result: boolean; errors: Error[] }[] = await Promise.all(resultPromises)
-    //   .then((values: { result: boolean; errors: Error[] }[]) => values)
-    //   .catch((err: Error) => {
-    //     console.log(err)
-
-    //     return [ { result: false, errors } ]
-    //   })
 
     const results: boolean[] = []
     const errors: Error[] = []
 
-    await Promise.all(resultPromises).then((promises: { result: boolean; errors: Error[] | [] }[]) => {
-      promises.map((promise: { result: boolean; errors: Error[] | [] }) => {
-        results.push(promise.result)
-        errors.push(...promise.errors)
-      })
-    })
+    await Promise.all(resultPromises).then(
+      (promises: ValidatorReturnType[]) => {
+        promises.map((promise: ValidatorReturnType) => {
+          results.push(promise.result)
+          errors.push(...promise.errors)
+        })
+      }
+    )
 
     const result: boolean = isValidated(results.map((r: boolean) => r))
 
