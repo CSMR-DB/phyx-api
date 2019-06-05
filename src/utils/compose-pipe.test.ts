b56e48e9c3b321408f858774981c3de1528d675f
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-05 14:36:24 +0200

    added compile-time example showing the impact of proper Generic typechecking in pipe & compose

--- src/utils/compose-pipe.test.ts
+++ src/utils/compose-pipe.test.ts
@@ -4,10 +4,13 @@ const add10: (v: number) => number = (v: number): number => v + 10
 
 const times2: (v: number) => number = (v: number): number => v * 2
 
+// const uppercase: (v: string) => string = (v: string): string =>
+//   v.toLocaleUpperCase()
+
 describe("compose()", () => {
   it("should compose functions RTL", () => {
     expect(
-      compose<number>(
+      compose(
         add10,
         times2
       )(4)
@@ -18,10 +21,22 @@ describe("compose()", () => {
 describe("pipe()", () => {
   it("should pipe functions LTR", () => {
     expect(
-      pipe<number>(
+      pipe(
         add10,
         times2
       )(4)
     ).toBe(28)
   })
 })
+
+// describe("pipe & compose compile time validation using Generics", () => {
+//   it("should give a compile time error", () => {
+//     expect(
+//       pipe(
+//         add10,
+//         times2,
+//         uppercase // <-- errors as expected. Consider adding a runtime implementation.
+//       )(4)
+//     ).toBe(null)
+//   })
+// })
