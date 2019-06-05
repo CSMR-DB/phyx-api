0495c4863202931bce3bc29764b16cbcfffb544d
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-05 15:14:02 +0200

    added test case using a string to compose-pipe

--- src/utils/compose-pipe.test.ts
+++ src/utils/compose-pipe.test.ts
@@ -4,11 +4,14 @@ const add10: (v: number) => number = (v: number): number => v + 10
 
 const times2: (v: number) => number = (v: number): number => v * 2
 
-// const uppercase: (v: string) => string = (v: string): string =>
-//   v.toLocaleUpperCase()
+const uppercase: (v: string) => string = (v: string): string =>
+  v.toLocaleUpperCase()
+
+const removeSymbols: (v: string) => string = (v: string): string =>
+  v.replace(/\W/gi, "")
 
 describe("compose()", () => {
-  it("should compose functions RTL", () => {
+  it("should compose functions RTL (number)", () => {
     expect(
       compose(
         add10,
@@ -16,16 +19,34 @@ describe("compose()", () => {
       )(4)
     ).toBe(18)
   })
+
+  it("should compose functions RTL (string)", () => {
+    expect(
+      compose(
+        removeSymbols,
+        uppercase
+      )("abcd1234&*^%")
+    ).toBe("ABCD1234")
+  })
 })
 
 describe("pipe()", () => {
-  it("should pipe functions LTR", () => {
+  it("should pipe functions LTR (number)", () => {
     expect(
       pipe(
         add10,
         times2
       )(4)
     ).toBe(28)
+  })
+
+  it("should pipe functions LTR (string)", () => {
+    expect(
+      pipe(
+        removeSymbols,
+        uppercase
+      )("abcd1234&*^%")
+    ).toBe("ABCD1234")
   })
 })
 
