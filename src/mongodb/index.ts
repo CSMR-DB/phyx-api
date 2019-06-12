806bc26c26e8d6411e4d214474aa9341022f9ae1
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-12 12:07:13 +0200

    removed hardcoded DB_CONN in favor of using a .env file

--- src/mongodb/index.ts
+++ src/mongodb/index.ts
@@ -1,10 +1,10 @@
 import { connect } from 'mongoose'
 
+require('dotenv').config()
+
 class Database {
-  private readonly DBURL: string =
-    process.env.DB_ROOT_URL ||
-    'mongodb://admin:xqiz8SMSwVH7T5v@ds223605.mlab.com:23605'
-  private readonly DBNAME: string = process.env.DB_COLLECTION_NAME || 'phyx'
+  private readonly DBURL: string | undefined = process.env.DB_ROOT_URL
+  private readonly DBNAME: string | undefined = process.env.DB_COLLECTION_NAME
 
   constructor() {
     this._connect()
