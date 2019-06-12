179cb330bc2a04386c79514d7658fd101bbbfa63
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-12 12:17:24 +0200

    added '_test' suffix to DB_COLLECTION_NAME in test files where MongoDB is involved

--- src/features/csgo/mongodb/csgo-strategy.mongodb.model.test.ts
+++ src/features/csgo/mongodb/csgo-strategy.mongodb.model.test.ts
@@ -31,7 +31,8 @@ describe('CSGO Strategy MongoDB Model', () => {
     if (mongoose.connection.readyState === 0) {
       mongoose
         .connect(
-          `mongodb://localhost:27017/${process.env.DB_COLLECTION_NAME}`,
+          `mongodb://localhost:27017/${process.env.DB_COLLECTION_NAME +
+            '_test'}`,
           { useNewUrlParser: true }, // <------- IMPORTANT
           function(err: MongoError): Function {
             if (err) {
