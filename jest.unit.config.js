02180ded0b09b8f49140f8b2881e42198cd01ccc
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-14 17:09:12 +0200

    added preset config files for jest unit & integration testing

---
+++ jest.unit.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  roots: ['src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      diagnostics: true
+    }
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '~src/(.*)': '<rootDir>/src/$1',
+    '~csgo/(.*)': '<rootDir>/src/features/csgo/$1',
+    '~r6siege/(.*)': '<rootDir>/src/features/r6siege/$1'
+  }
+}
