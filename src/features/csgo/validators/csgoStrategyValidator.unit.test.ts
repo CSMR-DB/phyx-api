d75c631d0cb64ae820aa8efa5f191e280dd08f61
Casimir de Bruijn <casimir.db@gmail.com>
2019-06-17 13:04:10 +0200

    added unit test to preset csgoStrategyValidator

---
+++ src/features/csgo/validators/csgoStrategyValidator.unit.test.ts
@@ -0,0 +1,11 @@
+import { csgoStrategyValidator } from './csgoStrategyValidator'
+import { csgoStrategyValid } from '../mocks/csgoStrategyValid.mock'
+
+describe('csgoStrategyValidator', () => {
+  test('should return valid when supplied with a valid strategy', async () => {
+    await expect(csgoStrategyValidator(csgoStrategyValid)).resolves.toEqual({
+      result: true,
+      errors: []
+    })
+  })
+})
